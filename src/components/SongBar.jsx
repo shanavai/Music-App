06d284f962e9749fdd9e9731d50ff3c0c9999d51
i <iraklishanava01@gmail.com>
2022-09-26 21:04:47 +0400

    Discover page >> Set up discover page where users can play top charting songs using Shazam core API and Redux toolkit

---
+++ src/components/SongBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SongBar = () => (
+  <div>SongBar</div>
+);
+
+export default SongBar;
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+
+import SongCard from './SongCard';
+import SongBar from './SongBar';
+import MusicPlayer from './MusicPlayer';
+import Loader from './Loader';
+import Error from './Error';
+
+export {
+  SongCard,
+  SongBar,
+  MusicPlayer,
+  Loader,
+  Error,
+};
